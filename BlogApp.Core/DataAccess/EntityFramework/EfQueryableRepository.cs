175a02945ee7ecb1fa31f8f99071881842aa5691
Serhat guclu <serhatguclu@gmail.com>
2022-07-10 16:37:59 +0300

    EfQueryableRepository implementasyonu gerçekleşmiştir.

--- BlogApp.Core/DataAccess/EntityFramework/EfQueryableRepository.cs
+++ BlogApp.Core/DataAccess/EntityFramework/EfQueryableRepository.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BlogApp.Core.Entity;
 
 namespace BlogApp.Core.DataAccess.EntityFramework
 {
-   public   class EfQueryableRepository
+    public class EfQueryableRepository<T> : IQueryableRepository<T> where T : class, IEntity, new()
     {
+        private DbContext _context;
+        private IDbSet<T> _entities;
+
+        public EfQueryableRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> Table => this.Entities;
+    
+
+        protected virtual IDbSet<T> Entities
+        {
+            get
+            {
+                if (_entities == null)
+                {
+                    _entities=_context.Set<T>();
+                }
+
+                return _entities;
+            }
+        }
     }
 }
